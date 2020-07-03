bda629e0ee14aa30a03c582f445d42a6c90a5475
JamieDawson <jamierobertdawson@gmail.com>
2020-07-02 18:01:11 -0700

    move zeros

---
+++ 283_moveZeroes/moveZeros.js
@@ -0,0 +1,18 @@
+
+
+var moveZeroes = function(nums) {
+    var count = 0;
+
+    for (var i = 0; i < nums.length; i++) {
+         while (nums[i] == 0){
+            nums.splice(i,1);
+            count++;
+        };
+    };
+    for (var i = 0; i < count; i++){
+        nums.push(0);
+    }
+};
+
+var testArray = [0,1,0,3,12]
+moveZeroes(testArray);
