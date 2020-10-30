c5cb7e671e58804dcabd4b281e8a0b765a945516
JamieDawson <jamierobertdawson@gmail.com>
2020-10-29 19:36:21 -0700

    first unique character in a string

---
+++ 387_firstUniqueCharacterInAString/firstUniqueCharacterInAString.js
@@ -0,0 +1,18 @@
+var firstUniqChar = function (s) {
+	const characterCount = {};
+
+	//Fill out character count object
+	for (let i = 0; i < s.length; i++) {
+		const char = s[i];
+		characterCount[char] = characterCount[char] + 1 || 1;
+	}
+
+	for (let i = 0; i < s.length; i++) {
+		const char = s[i];
+		if (characterCount[char] === 1) {
+			return i;
+		}
+	}
+
+	return -1;
+};
