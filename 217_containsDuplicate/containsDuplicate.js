87bb8601288dd834808ec5b6938102572377e489
JamieDawson <jamierobertdawson@gmail.com>
2020-11-06 17:12:06 -0800

    added hashmap to containsDuplicate

--- 217_containsDuplicate/containsDuplicate.js
+++ 217_containsDuplicate/containsDuplicate.js
@@ -1,1 +1,19 @@
-var containsDuplicate = function (nums) {};
+var containsDuplicate = function (nums) {
+	let hash = {};
+
+	for (var i = 0; i < nums.length; i++) {
+		let key = nums[i];
+		hash[key] = hash[key] + 1 || 1;
+	}
+
+	for (var i = 0; i < nums.length; i++) {
+		let key = nums[i];
+		if (hash[key] >= 2) {
+			return true;
+		}
+	}
+	return false;
+};
+
+let arr = [1, 2, 3];
+console.log(containsDuplicate(arr));
