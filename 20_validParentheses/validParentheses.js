568ed4e0f832f6ae25bb2e346e4f1cb346551361
JamieDawson <jamierobertdawson@gmail.com>
2020-11-30 22:34:48 -0800

    valid parentheses. I need to clean this up

---
+++ 20_validParentheses/validParentheses.js
@@ -0,0 +1,28 @@
+var isValid = function (s) {
+	const stack = [];
+
+	for (let char of s) {
+		if (char == '(' || char == '[' || char == '{') {
+			stack.push(char);
+		} else {
+			if (stack.length === 0) return false;
+			const last = stack.pop();
+			if (last === '(' && char !== ')') {
+				return false;
+			} else if (last === '[' && char !== ']') {
+				return false;
+			} else if (last === '{' && char !== '}') {
+				return false;
+			}
+		}
+	}
+	if (stack.length) {
+		return false;
+	} else {
+		return true;
+	}
+};
+
+let s = '([{}])';
+
+isValid(s);
