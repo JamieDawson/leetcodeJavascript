4671dea25021cfd5359f5497cbee3f8801a11f47
JamieDawson <jamierobertdawson@gmail.com>
2020-12-01 21:01:57 -0800

    updated code

--- 35_searchInsertPosition/searchInsertPosition.js
+++ 35_searchInsertPosition/searchInsertPosition.js
@@ -3,9 +3,9 @@ var searchInsert = function (nums, target) {
 	let hi = nums.length - 1;
 
 	while (lo <= hi) {
-		let mid = Math.floor((hi + lo) / 2);
+		let mid = Math.floor((lo + hi) / 2);
 		let midVal = nums[mid];
-		if (target === midVal) {
+		if (midVal == target) {
 			return mid;
 		} else if (target > midVal) {
 			lo = mid + 1;
