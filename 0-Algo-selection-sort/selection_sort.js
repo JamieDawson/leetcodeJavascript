ea8aec39ff903a778691ec21f82ab14938f2bd70
JamieDawson <jamierobertdawson@gmail.com>
2021-02-16 21:53:37 -0800

    selection sort

---
+++ 0-Algo-selection-sort/selection_sort.js
@@ -0,0 +1,20 @@
+let selectionSort = (arr) => {
+	for (var i = 0; i < arr.length; i++) {
+		let min = i;
+		for (var j = i + 1; arr.length; j++) {
+			if (arr[min] > arr[j]) {
+				min = j;
+			}
+		}
+		if (min !== i) {
+			let temp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = temp;
+		}
+	}
+	console.log(arr);
+	return arr;
+};
+
+let unsortedArray = [3, 2, 4, 1];
+selectionSort(unsortedArray);
