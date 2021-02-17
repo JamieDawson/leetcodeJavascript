0ed82b5c760661665c990a2061c4831c6be62472
JamieDawson <jamierobertdawson@gmail.com>
2021-02-16 22:03:49 -0800

    updated code

--- 0-Algo-selection-sort/selection_sort.js
+++ 0-Algo-selection-sort/selection_sort.js
@@ -1,7 +1,7 @@
 let selectionSort = (arr) => {
 	for (var i = 0; i < arr.length; i++) {
-		let min = i;
-		for (var j = i + 1; arr.length; j++) {
+		var min = i;
+		for (var j = i + 1; j < arr.length; j++) {
 			if (arr[min] > arr[j]) {
 				min = j;
 			}
