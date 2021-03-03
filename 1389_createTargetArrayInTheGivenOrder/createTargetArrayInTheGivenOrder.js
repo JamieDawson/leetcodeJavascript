fbd6ab50f72229e0ea50fb38e8b38730a8c1207e
JamieDawson <jamierobertdawson@gmail.com>
2021-03-03 15:55:14 -0800

    first solution. but not fully working

--- 1389_createTargetArrayInTheGivenOrder/createTargetArrayInTheGivenOrder.js
+++ 1389_createTargetArrayInTheGivenOrder/createTargetArrayInTheGivenOrder.js
@@ -1,23 +1,24 @@
 var createTargetArray = function (nums, index) {
 	var kv = {};
+	var sorted = [];
 
 	for (var i = 0; i < index.length; i++) {
-        if (kv[index[i]]) 
-	}
-   // console.log(index[i]); //get index item
+		const grab = index[i];
 
-	//console.log(kv);
+		if (kv[grab]) kv[grab].push(nums[i]);
+		else kv[grab] = [nums[i]];
+	}
+
+	for (var i in kv) {
+		kv[i].reverse();
+		for (var j = 0; j < kv[i].length; j++) {
+			sorted.push(kv[i][j]);
+		}
+	}
+	return sorted;
 };
 
 let nums = [0, 1, 2, 3, 4];
 let index = [0, 1, 2, 2, 1];
 
 createTargetArray(nums, index);
-
-/*
-if kv index exist
-    push value in it
-else
-    create index and add value.
-
-*/
