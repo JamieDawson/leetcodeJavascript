760a50d1525c777c7b7959f9d28b3416fce8cd95
JamieDawson <jamierobertdawson@gmail.com>
2021-03-02 20:46:14 -0800

    Need to finalize solution for 1389

---
+++ 1389_createTargetArrayInTheGivenOrder/createTargetArrayInTheGivenOrder.js
@@ -0,0 +1,23 @@
+var createTargetArray = function (nums, index) {
+	var kv = {};
+
+	for (var i = 0; i < index.length; i++) {
+        if (kv[index[i]]) 
+	}
+   // console.log(index[i]); //get index item
+
+	//console.log(kv);
+};
+
+let nums = [0, 1, 2, 3, 4];
+let index = [0, 1, 2, 2, 1];
+
+createTargetArray(nums, index);
+
+/*
+if kv index exist
+    push value in it
+else
+    create index and add value.
+
+*/
