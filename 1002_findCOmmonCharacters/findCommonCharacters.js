4de2466fa1e0ee04afd306293fc40aa73eb32906
JamieDawson <jamierobertdawson@gmail.com>
2021-03-03 18:52:10 -0800

    work on logic for comparing

---
+++ 1002_findCOmmonCharacters/findCommonCharacters.js
@@ -0,0 +1,24 @@
+// var commonChars = function(A) {
+
+// };
+
+var commonChars = (A) => {
+	console.log(A);
+};
+
+var A = ['bella', 'label', 'roller'];
+commonChars(A);
+
+/*
+Create main hash
+create updating hash for comparing
+
+loop #1: create main hash with first word in array
+
+loop: create has with "updated" hash
+    loop: compare values
+        if key in main doesn't exist in update hash
+            remove it
+        
+
+*/
