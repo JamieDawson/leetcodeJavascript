09a57289ef6259762809760af2f38ea24a101b83
JamieDawson <jamierobertdawson@gmail.com>
2021-07-17 12:51:52 -0700

    updated reverse linked list

--- 206_reverseLinkedList/reverseLinkedList.js
+++ 206_reverseLinkedList/reverseLinkedList.js
@@ -1,13 +1,39 @@
+function NodeList(val) {
+	this.val = val;
+	this.next = null;
+}
+
 var reverseList = function (head) {
 	let prev = null;
-	let curr = head;
-	let nextTemp = null;
-
-	while (curr != null) {
-		nextTemp = curr.next;
-		curr.next = prev;
-		prev = curr;
-		curr = nextTemp;
+	let on = head;
+	while (on !== null) {
+		let temp = on.next;
+		on.next = prev;
+		prev = on;
+		on = temp;
 	}
 	return prev;
 };
+
+//tail recursion
+var reverseListR = function (on, prev = null) {
+	if (on === null) return prev;
+
+	let temp = on.next;
+	on.next = prev;
+
+	console.log(JSON.stringify(temp));
+	console.log(JSON.stringify(on));
+	console.log(`\n`);
+
+	return reverseListR(temp, on);
+};
+
+let node1 = new NodeList(1);
+let node2 = new NodeList(2);
+let node3 = new NodeList(3);
+node1.next = node2;
+node2.next = node3;
+let test = null;
+
+reverseListR(node1, test);
