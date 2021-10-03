19e67c50682ad6f8b7cf1bde993e0943bd14c4a2
JamieDawson <jamierobertdawson@gmail.com>
2021-10-03 14:42:23 -0700

    updating 26

--- 26_removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.js
+++ 26_removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.js
@@ -1,18 +1,36 @@
 var removeDuplicates = function (nums) {
-	let small = nums[0];
-	let big = nums[nums.length - 1];
+	let first = nums[0];
+	let last = nums[nums.length - 1];
+	let k = 0;
+
+	while (first <= last) {
+		k++;
+		first++;
+	}
+
+	first = nums[0];
 
 	for (var i = 0; i < nums.length; i++) {
-		if (small <= big) {
-			nums[i] = small;
-			small++;
+		if (first <= last) {
+			nums[i] = first;
+			first++;
 		} else {
 			nums[i] = '_';
 		}
 	}
-	console.log(nums[big]);
-	return nums[big];
+	//console.log(nums);
+
+	return k;
 };
 
 var nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+
 removeDuplicates(nums);
+
+/*
+Inputs you can get
+[0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+[1,1,2];
+[0,0,0,0,3];
+
+*/
