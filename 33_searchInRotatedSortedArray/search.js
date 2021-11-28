84991a475edc6233a90d5e7e39e617556b027928
unknown <jamierobertdawson@gmail.com>
2021-11-27 18:20:11 -0800

    working on solution

---
+++ 33_searchInRotatedSortedArray/search.js
@@ -0,0 +1,16 @@
+var search = function(nums, target) {
+    var middle = (nums.length - 1) / 2;
+    console.log(Math.round(middle));
+};
+
+var nums = [4,5,6,7,0,1,2];
+var target = 0;
+
+search(nums, target);
+
+
+/*
+check right side:
+if target is less than or equal to middle-1
+
+*/
