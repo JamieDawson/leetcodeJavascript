5e768c8d42e568884fd918fe7f919b86b85bf9c6
unknown <jamierobertdawson@gmail.com>
2022-05-01 22:16:35 -0700

    work on logic

--- 1773_countItemsMatchingARulle/countItemsMatchingARule.js
+++ 1773_countItemsMatchingARulle/countItemsMatchingARule.js
@@ -1,23 +1,46 @@
 var countMatches = function (items, ruleKey, ruleValue) {
-	//counter variable
-	//while items length
-	//if items[i][0] is rule value = increase varaible.
+	let counter = 0;
+	let rule = -1;
 
-	let count = 0;
-	for (var i = 0; i < items.length; i++) {
-		if (items[i][0] == ruleValue) {
-			count++;
+	switch(ruleKey) {
+		case "type":
+			rule = 0;
+			break;
+		case "color":
+			rule = 1;
+			break;
+		case "name":
+			rule = 2;
+			break;
+	}
+
+	for(var i = 0; i < items.length; i++) {
+		if (items[i][rule] == ruleValue) {
+			counter++;
 		}
 	}
-	return count++;
+	return counter;
 };
 
-let items = [
-	['phone', 'blue', 'pixel'],
-	['computer', 'silver', 'lenovo'],
-	['phone', 'gold', 'iphone'],
-];
-let ruleKey = 'color';
-let ruleValue = 'silver';
+let items = [["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]]
+let ruleKey = 'type';
+let ruleValue = 'phone';
 
 countMatches(items, ruleKey, ruleValue);
+
+
+/*
+counter variable: counts items in correct spot
+
+Create rule variable call rule (type = 0, color = 1, and name = 2)
+
+loop through array, count with i
+	if arrayLocation[i][rule] is equal to ruleValue
+		counter++
+
+return counter
+
+
+Time compleity: O(n): scan through the length of the array. 
+Space complexity: O(1): Not creating extra space. Only creating single number variable.
+*/
---
+++ 2037_minimumNumberOfMovesToSeatEveryone/mini.js
@@ -0,0 +1,16 @@
+var minMovesToSeat = function(seats, students) {
+    
+};
+
+let seats = [3,1,5];
+let students = [2,7,4];
+
+minMovesToSeat(seats, students)
+
+
+/*
+Use .sort() on seast and students
+
+Loop through 
+
+*/
