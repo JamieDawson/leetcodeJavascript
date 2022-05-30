ff3b1169adfac5837c250e3643ae77625a3a18ca
unknown <jamierobertdawson@gmail.com>
2022-05-30 15:45:52 -0700

    minimum number of moves to seat everyone

--- 2037_minimumNumberOfMovesToSeatEveryone/mini.js
+++ 2037_minimumNumberOfMovesToSeatEveryone/mini.js
@@ -1,16 +1,34 @@
 var minMovesToSeat = function(seats, students) {
+    let moves = 0;
+    seats.sort((a, b) => a - b);
+    students.sort((a, b) => a - b)
     
+    for(var i = 0; i < seats.length; i++) {
+        moves += Math.abs(seats[i] - students[i])
+    }
+
+    return moves;
 };
 
-let seats = [3,1,5];
-let students = [2,7,4];
+let seats = [4,1,5,9];
+let students = [1,3,2,6];
 
 minMovesToSeat(seats, students)
 
 
 /*
 Use .sort() on seast and students
+varaible for total moves
 
-Loop through 
+Loop through student array
+    compar student value to seat value.
+        
+            
+return moves
+*/
+
+/*
+Time: O(n log(n)) since we are using sort() function.
+Space: O(1), just making a variable to store moves.
 
 */
