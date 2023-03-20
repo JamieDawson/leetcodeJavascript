ef4be5aa8fd79292f238cef52f9aedc1d712076e
unknown <jamierobertdawson@gmail.com>
2023-03-20 13:56:12 -0700

    wokred on new solution for 867 middle of the linked list

--- 867_middleOfTheLinkedList/middle.js
+++ 867_middleOfTheLinkedList/middle.js
@@ -1,52 +1,46 @@
 function NodeList(val) {
-	this.val = val;
-	this.next = null;
+  this.val = val;
+  this.next = null;
 }
 
-// function isEven(value) {
-// 	if (value % 2 == 0) return true;
-// 	else return false;
-// }
+function isEven(value) {
+  let nodeToFindLength = value;
+  let counter = 0;
+  let actualMiddleLenght;
+  let headNode = value;
+
+  while (nodeToFindLength !== null) {
+    nodeToFindLength = nodeToFindLength.next;
+    counter++;
+  }
+
+  actualMiddleLenght = Math.round(counter / 2) - (counter % 2);
+  counter = 0;
+
+  while (counter < actualMiddleLenght) {
+    headNode = headNode.next;
+    counter++;
+  }
+
+  return headNode;
+}
 
 // var middleNode = function (head) {
-// 	let counter = 0;
-// 	let tempHead = head;
-
-// 	while (tempHead != null) {
-// 		tempHead = tempHead.next;
-// 		counter++;
+// 	slow = fast = head;
+// 	while (fast && fast.next) {
+// 		slow = slow.next;
+// 		fast = fast.next.next;
 // 	}
-
-// 	// let final = isEven(counter);
-// 	// console.log(final);
-
-// 	counter = Math.round(counter / 2);
-// 	tempHead = head;
-
-// 	for (var i = 0; i < counter; i++) {
-// 		tempHead = tempHead.next;
-// 	}
-
-// 	//console.log(tempHead);
-// 	return tempHead;
+// 	console.log(slow);
+// 	return slow;
 // };
-
-var middleNode = function (head) {
-	slow = fast = head;
-	while (fast && fast.next) {
-		slow = slow.next;
-		fast = fast.next.next;
-	}
-	console.log(slow);
-	return slow;
-};
 
 let node1 = new NodeList(1);
 let node2 = new NodeList(2);
 let node3 = new NodeList(3);
 let node4 = new NodeList(4);
 let node5 = new NodeList(5);
-let node6 = new NodeList(6);
+//let node6 = new NodeList(6);
 
 node1.next = node2;
 node2.next = node3;
@@ -54,4 +48,4 @@ node3.next = node4;
 node4.next = node5;
 node5.next = node6;
 
-middleNode(node1);
+console.log(isEven(node1));
