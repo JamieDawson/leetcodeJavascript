9d2cb7b704ffe835abe7c0ca06be1d413183e712
Jamie Dawson <jamierobertdawson@gmail.com>
2023-09-05 02:47:56 -0700

    167 two Sum II

---
+++ 167_twoSumII/twoSumII.js
@@ -0,0 +1,23 @@
+var twoSum = function (numbers, target) {
+  let left = 0;
+  let right = numbers.length - 1;
+
+  while (left < right) {
+    let sum = numbers[left] + numbers[right];
+    if (sum === target) {
+      return [left + 1, right + 1];
+    }
+    if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+};
+
+let numbers = [2, 7, 11, 15];
+let target = 9;
+
+console.log(twoSum(numbers, target));
+
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/solutions/2799628/easy-clear-and-classic-javascript-solution-two-sum-ii-input-array-is-sorted/
