40bb7f4484679cce308da3989a035f8c5c5276f5
Jamie Dawson <jamierobertdawson@gmail.com>
2024-07-04 23:54:00 -0700

    updated bfs with better solution

--- algorithms/bfsWith2Dmatrix/bfswith2dmatrix.js
+++ algorithms/bfsWith2Dmatrix/bfswith2dmatrix.js
@@ -1,33 +1,52 @@
-const bfs = (grid, start) => {
-  let rowSize = grid.length;
-  let colSize = grid[0].length;
-  let visited = Array(rowSize).fill([]);
-  let directions = [
-    [-1, 0],
-    [1, 0],
-    [0, -1],
-    [0, 1],
-  ];
+const bfs = (grid, start, target) => {
+  let ROWS = grid.length;
+  let COLS = grid[0].length;
+  let visited = new Set([start].toString());
+  let queue = [start];
+  let length = 0;
 
-  //fill the visited array with all false values.
-  for (var i = 0; i < rowSize; i++) {
-    for (var j = 0; j < colSize; j++) {
-      visited[i][j] = "false";
+  while (queue.length > 0) {
+    let size = queue.length;
+
+    for (let i = 0; i < size; i++) {
+      let [r, c] = queue.shift();
+
+      if (target[0] === r && target[1] === c) {
+        return length;
+      }
+
+      const directions = [
+        [r + 1, c],
+        [r - 1, c],
+        [r, c + 1],
+        [r, c - 1],
+      ];
+
+      for (let [row, col] of directions) {
+        if (
+          row >= 0 &&
+          row < ROWS &&
+          col >= 0 &&
+          col < COLS &&
+          !visited.has([row, col].toString()) &&
+          grid[row][col] === 0
+        ) {
+          queue.push([row, col]);
+          visited.add([row, col].toString());
+        }
+      }
     }
+    length++;
   }
-
-  console.log(visited);
+  return -1;
 };
 
+// Example usage:
 const grid = [
-  [0, 1, 0, 0, 0],
-  [0, 1, 0, 1, 0],
-  [0, 0, 0, 1, 0],
-  [0, 1, 0, 0, 0],
-  [0, 0, 0, 1, 0],
+  [0, 0, 0, 0],
+  [1, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 1, 0, 0],
 ];
 
-const start = [0, 0]; // Start at the top-left corner (row 0, column 0)
-
-// Run BFS
-bfs(grid, start);
+console.log(bfs(grid, [0, 0], [3, 3])); // Output: 6
