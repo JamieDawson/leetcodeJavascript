3bedf3e4260781a67e87f34c5e1d003b30a83aa2
JamieDawson <jamierobertdawson@gmail.com>
2026-01-06 21:09:03 -0800

    updated code to make it easier to read

--- 567_permutationInString/perm.js
+++ 567_permutationInString/perm.js
@@ -1,58 +1,54 @@
-let createS1Hash = (s1) => {
-  let s1Hash = {};
-  for (let char of s1) {
-    s1Hash[char] = (s1Hash[char] || 0) + 1;
-  }
-  return s1Hash;
-};
-
-const updateS2Hash = (s2Hash, s2, i, j) => {
-  // Add new character entering the window (j-th position)
-  s2Hash[s2[j]] = (s2Hash[s2[j]] || 0) + 1;
-
-  // Remove character that's leaving the window (i-th position)
-  if (i >= 0) {
-    s2Hash[s2[i]]--;
-    if (s2Hash[s2[i]] === 0) delete s2Hash[s2[i]]; // Clean up zero entries
-  }
-
-  return s2Hash;
-};
-
-const checkMatches = (s1Hash, s2Hash) => {
-  for (const key in s1Hash) {
-    if (s1Hash[key] !== s2Hash[key]) {
-      return false; // Return false if any character frequency mismatches
-    }
+const matches = (an, b) => {
+  for (let v in an) {
+    if (an[v] !== b[v]) return false;
   }
   return true;
 };
 
-var checkInclusion = function (s1, s2) {
-  if (s1.length > s2.length) return false; // If s1 is longer, s2 can't contain a permutation
+function checkInclusion(s1: string, s2: string): boolean {
+  let need = {};
+  let window = {};
+  let left = 0;
 
-  let i = -1; // Start i out of bounds for the first window
-  let j = 0;
-  let s1Hash = createS1Hash(s1);
-  let s2Hash = {}; // Empty hash initially
-
-  while (j < s2.length) {
-    s2Hash = updateS2Hash(s2Hash, s2, i, j); // Slide window and update s2Hash
-
-    if (j - i === s1.length) {
-      // Window size equals s1's length
-      if (checkMatches(s1Hash, s2Hash)) {
-        return true; // Found a permutation match
-      }
-      i++; // Slide window forward
+  for (let v of s1) {
+    if (!need[v]) {
+      need[v] = 1;
+    } else {
+      need[v]++;
     }
-    j++; // Move the end of the window
+  }
+
+  for (let v of s2) {
+    if (!window[v]) {
+      window[v] = 0;
+    }
+  }
+
+  for (let right = 0; right < s2.length; right++) {
+    //grab current value
+    let curr = s2[right];
+
+    //push it into window
+    window[curr]++;
+
+    //if right - left + 1 > s1.length
+    //grab left value
+    //decrase it from window
+    //left++
+
+    if (right - left + 1 > s1.length) {
+      let leftCurr = s2[left];
+      window[leftCurr]--;
+      left++;
+    }
+
+    //if right - left + 1 is equal to s1.length
+    //check matches with need & window
+    if (right - left + 1 === s1.length) {
+      let check = matches(need, window);
+      if (check === true) return true;
+    }
   }
 
   return false;
-};
-
-// Test the function
-let s1 = "ab";
-let s2 = "eidbaooo";
-console.log(checkInclusion(s1, s2)); // Output: true
+}
