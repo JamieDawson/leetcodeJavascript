b95f9b5b726f024d76de0bff6a0a887bc48382be
JamieDawson <jamierobertdawson@gmail.com>
2026-03-07 20:12:00 -0800

    updated logic for 53

--- 53_maximumSubarray/max.js
+++ 53_maximumSubarray/max.js
@@ -1,13 +1,16 @@
-var maxSubArray = function (nums) {
-  let maxSub = nums[0];
-  let curSum = 0;
+function maxSubArray(nums: number[]): number {
+    let sum = 0;          // running sum of current subarray
+    let max = nums[0];    // biggest sum seen so far
 
-  for (var i = 0; i < nums.length; i++) {
-    if (curSum < 0) {
-      curSum = 0;
+    for (let n of nums) { // loop through numbers
+        sum += n;         // add current number to running sum
+
+        max = Math.max(max, sum); // update max if this sum is bigger
+
+        if (sum < 0) {    // if running sum becomes negative
+            sum = 0;      // reset (negative sums hurt future subarrays)
+        }
     }
-    curSum += nums[i];
-    maxSub = Math.max(maxSub, curSum);
-  }
-  return maxSub;
-};
+
+    return max;           // return the biggest subarray sum found
+}
