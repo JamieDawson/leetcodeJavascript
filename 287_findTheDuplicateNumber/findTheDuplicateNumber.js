20ebe69c86eea88b9fc4d08e69d88f79b2da8560
JamieDawson <jamierobertdawson@gmail.com>
2026-04-12 11:00:17 -0700

    chore: Updated 287

--- 287_findTheDuplicateNumber/findTheDuplicateNumber.js
+++ 287_findTheDuplicateNumber/findTheDuplicateNumber.js
@@ -1,24 +1,23 @@
 var findDuplicate = function (nums) {
   let slow = nums[0];
-  let fast = nums[nums[0]];
+  let fast = nums[0];
 
-  while (slow != fast) {
-    // we are guaranteed to have a cycle
-    console.log("first loop");
-    console.log(fast);
+  // force first move
+  slow = nums[slow];
+  fast = nums[nums[fast]];
+
+  // now safe to compare
+  while (slow !== fast) {
     slow = nums[slow];
     fast = nums[nums[fast]];
   }
 
-  slow = 0;
-
-  while (slow != fast) {
+  // Step 2
+  slow = nums[0];
+  while (slow !== fast) {
     slow = nums[slow];
     fast = nums[fast];
   }
 
   return slow;
 };
-
-let nums = [1, 3, 4, 3, 2];
-findDuplicate(nums);
